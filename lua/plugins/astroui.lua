259d6d8b5874b7a0a9bfbe76c5dfccf0d34eb3aa
Mae <45900436+cadrianmae@users.noreply.github.com>
2026-07-23 15:23:04 +0100

    feat: lumae theme via tinted-nvim (auto-follows tinty)

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -11,7 +11,7 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "catppuccin",
+    colorscheme = nil, -- lumae owns it via tinted-nvim (plugins/tinted-lumae.lua)
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
