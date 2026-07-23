96bdbaf48baaa40ba89f46199f8504248e5ea9a7
Mae <45900436+cadrianmae@users.noreply.github.com>
2026-07-23 15:24:16 +0100

    feat(nvim): minecraft datapack plugin

---
+++ lua/plugins/minecraft-datapack.lua
@@ -0,0 +1,34 @@
+-- Minecraft datapack support (beet / mecha / bolt + Spyglass LSP)
+-- Used by: ~/git/github.com/cadrianmae/Advancement-Count
+
+---@type LazySpec
+return {
+  -- Filetype detection + exrc for project-local .nvim.lua
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      options = {
+        opt = {
+          exrc = true,
+        },
+      },
+      filetypes = {
+        extension = {
+          bolt = "python", -- bolt = Python-superset DSL
+          mcfunction = "mcfunction", -- Spyglass attaches here
+        },
+      },
+    },
+  },
+
+  -- Register Spyglass LSP (install via :MasonInstall spyglassmc-language-server)
+  -- Handles: .mcfunction, .json (advancements/tags/loot/etc), .mcmeta, .nbt, .snbt
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      servers = { "spyglassmc_language_server" },
+    },
+  },
+}
